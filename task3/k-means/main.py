42a725597f95f824cc766478308e10600582f09c
petr-shumilov <pilot-shumilov@yandex.ru>
2019-01-16 00:14:09 +0300

    Refactoring

--- task3/k-means/main.py
+++ task3/k-means/main.py
@@ -4,45 +4,49 @@ import os
 
 from PIL import Image
 
+def k_means(data, clusters_num, max_iteration):
 
-def k_means(points, k, iterations):
+    def distance(x, y, axis=None): 
+        return np.linalg.norm(x - y, axis=axis)
 
-    n = points.shape[0]
-    centroids = points[np.random.randint(n, size=k),:]
+    data_size = data.shape[0]
 
-    new_centroids   = np.zeros(centroids.shape)
-    clusters        = np.zeros(n)
-    distances       = np.zeros((n, k))
+    random_subset_indicies = np.random.randint(low=0, high=data_size, size=clusters_num)
+    cluster_centers = data[random_subset_indicies, :]
+    cluster_by_item = np.zeros(data_size)
+    new_cluster_centers = np.zeros((clusters_num, 3))
+             
+    still_changed = True
+    iteration = 0
+    while iteration < max_iteration and still_changed:
+        norms = np.zeros((data_size, clusters_num))
+        for cluster in range(clusters_num):
+            norms[:, cluster] = distance(data, cluster_centers[cluster], axis=1)
+        
+        cluster_by_item = np.argmin(norms, axis=1)
+        
+        for cluster in range(clusters_num):
+            cluster_items = [data[item] for item, _cluster in enumerate(cluster_by_item) if cluster == _cluster]
+            if cluster_items:
+                new_cluster_centers[cluster] = np.mean(cluster_items, axis=0)
 
-    changed = True
-    while changed and iterations > 0:
-        for i in range(k):
-            distances[:, i] = np.linalg.norm(points - centroids[i], axis=1)
-
-        clusters = np.argmin(distances, axis=1)
-
-        for i in range(k):
-            new_centroids[i] = np.mean(points[clusters == i], axis=0)
-
-        changed = np.linalg.norm(centroids - new_centroids) != 0
-
-        centroids = deepcopy(new_centroids)
-        iterations -= 1
-
-    return centroids, clusters
+        still_changed = distance(cluster_centers, new_cluster_centers) != 0
+        cluster_centers = new_cluster_centers
+        iteration += 1
+        
+    return cluster_centers, cluster_by_item
 
 
 def main():
-    INPUT_DIR = '/homework/stochastic-course/task3/data' 
-    # INPUT_DIR = 'data' 
+    #INPUT_DIR = '/homework/stochastic-course/task3/data' 
+    INPUT_DIR = 'data' 
     
-    RESULT_DIR = '/homework/stochastic-course/task3/results'
-    # RESULT_DIR = 'results'
+    #RESULT_DIR = '/homework/stochastic-course/task3/results'
+    RESULT_DIR = 'results'
 
-    NUM_OF_CLUSTERS = [3, 5, 8]
-    NUM_OF_ITERATIONS = 300
-    NUM_OF_ATTEMPTS = 3
-
+    NUM_OF_CLUSTERS = [2, 3, 5, 10, 13, 17, 21]
+    NUM_OF_ITERATIONS = 5
+    
 
     os.makedirs(RESULT_DIR, exist_ok=True)
     files = [(os.path.splitext(filepath)[0], os.path.splitext(filepath)[1][1:]) for filepath in os.listdir(INPUT_DIR)]
@@ -56,14 +60,13 @@ def main():
         points = origin_img.reshape((origin_img.shape[0] * origin_img.shape[1], origin_img.shape[2]))
         
         for num_clusters in NUM_OF_CLUSTERS:
-            for attempt in range(NUM_OF_ATTEMPTS):
-
-                centroids, clusters = k_means(points, num_clusters, NUM_OF_ITERATIONS)
-                
-                compressed_img_url = os.path.join(RESULT_DIR, '{}_splitted_on_{}_attempt_{}.{}'.format(filename, num_clusters, attempt, fileext))
-                compressed_img = np.vstack([centroids[i] for i in clusters]).astype(np.uint8).reshape(origin_img.shape)
-                
-                Image.fromarray(compressed_img).save(compressed_img_url)
+            
+            cluster_centers, cluster_by_item = k_means(points, num_clusters, NUM_OF_ITERATIONS)
+            
+            compressed_img_url = os.path.join(RESULT_DIR, '{}_splitted_on_{}.{}'.format(filename, num_clusters, fileext))
+            compressed_img = np.vstack([cluster_centers[i] for i in cluster_by_item]).astype(np.uint8).reshape(origin_img.shape)
+            
+            Image.fromarray(compressed_img).save(compressed_img_url)
 
     print('DONE!')
 
